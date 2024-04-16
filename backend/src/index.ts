a23a66881ac35069b22b8c891cf8bef9df058828
Michal Sokolik <msokolik55@gmail.com>
2024-04-16 15:38:22 +0200

    refactor: moved socket parts into separate file in BE

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,109 +1,33 @@
 import cors from "cors";
 import express from "express";
 import { createServer } from "http";
-import { Socket, Server as SocketIOServer } from "socket.io";
+import { Server as SocketIOServer } from "socket.io";
 
 import { logInfo } from "./logger";
 import router from "./router";
+import { checkHeartbeat, registerCounter } from "./socket";
 
 const api = express();
 const httpServer = createServer(api);
 
 const corsPolicy = {
-	origin: "http://localhost:8080",
-	methods: ["GET", "POST"],
+	origin: "http://localhost:3000",
+	methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 };
-
-const io = new SocketIOServer(httpServer, {
-	cors: corsPolicy,
-});
-
-const port = process.env.PORT ?? 4000;
 
 api.use(express.json());
 api.use(cors(corsPolicy));
 api.use("/", router);
 
-type SocketData = {
-	streamKey: string;
-	browserId: string;
-};
-
-const streamViewers: { [streamId: string]: number } = {};
-const alreadyConnected: { [browserId: string]: number } = {};
-
-const disconnectUser = (socket: Socket) => {
-	alreadyConnected[socket.data.browserId]--;
-	if (alreadyConnected[socket.data.browserId] > 0) {
-		return;
-	}
-
-	console.log("A user disconnected");
-	delete alreadyConnected[socket.data.browserId];
-	streamViewers[socket.data.streamKey] = Math.max(
-		streamViewers[socket.data.streamKey] - 1,
-		0,
-	);
-
-	io.emit(
-		`viewer_count_${socket.data.streamKey}`,
-		streamViewers[socket.data.streamKey],
-	);
-	socket.disconnect();
-};
-
+const io = new SocketIOServer(httpServer, {
+	cors: corsPolicy,
+});
 io.on("connection", (socket) => {
-	socket.data = {
-		heartbeat: Date.now(),
-		streamKey: "",
-		browserId: "",
-	};
-
-	logInfo("io", "connection", "A user connected");
-
-	socket.on("join_stream", (data: SocketData) => {
-		if (!alreadyConnected[data.browserId]) {
-			alreadyConnected[data.browserId] = 0;
-			if (!streamViewers[data.streamKey]) {
-				streamViewers[data.streamKey] = 0;
-			}
-			streamViewers[data.streamKey]++;
-		}
-		alreadyConnected[data.browserId]++;
-
-		socket.data = data;
-		io.emit(
-			`viewer_count_${data.streamKey}`,
-			streamViewers[data.streamKey],
-		);
-	});
-
-	socket.on("heartbeat", (data: { heartbeat: number }) => {
-		socket.data.heartbeat = data.heartbeat;
-	});
-
-	socket.on("disconnect", () => {
-		disconnectUser(socket);
-	});
+	registerCounter(io, socket);
+	checkHeartbeat(io);
 });
 
-setInterval(() => {
-	io.sockets.sockets.forEach((socket) => console.log(socket.data));
-
-	const now = Date.now();
-	io.sockets.sockets.forEach((socket) => {
-		if (now - socket.data.heartbeat > 2000) {
-			logInfo(
-				"WS",
-				setInterval.name,
-				"No heartbeat received in the last 30 seconds. Assuming disconnected.",
-			);
-
-			disconnectUser(socket);
-		}
-	});
-}, 1000);
-
+const port = process.env.PORT ?? 4000;
 httpServer.listen(port, () =>
 	logInfo("httpServer", "listen", "Example app listening on port", port),
 );
---
+++ backend/src/socket.ts
@@ -0,0 +1,86 @@
+import { Server, Socket } from "socket.io";
+
+import { logInfo } from "./logger";
+
+type SocketData = {
+	streamKey: string;
+	browserId: string;
+};
+
+const streamViewers: { [streamId: string]: number } = {};
+const alreadyConnected: { [browserId: string]: number } = {};
+let connectedSockets: Socket[] = [];
+
+const disconnectUser = (io: Server, socket: Socket) => {
+	alreadyConnected[socket.data.browserId]--;
+	if (alreadyConnected[socket.data.browserId] > 0) {
+		return;
+	}
+
+	console.log("A user disconnected");
+	delete alreadyConnected[socket.data.browserId];
+	connectedSockets = connectedSockets.filter((s) => s !== socket);
+	streamViewers[socket.data.streamKey] = Math.max(
+		streamViewers[socket.data.streamKey] - 1,
+		0,
+	);
+
+	io.emit(
+		`viewer_count_${socket.data.streamKey}`,
+		streamViewers[socket.data.streamKey],
+	);
+	socket.disconnect();
+};
+
+export const registerCounter = (io: Server, socket: Socket) => {
+	socket.data = {
+		heartbeat: Date.now(),
+		streamKey: "",
+		browserId: "",
+	};
+
+	logInfo("io", "connection", "A user connected");
+
+	socket.on("join_stream", (data: SocketData) => {
+		if (!alreadyConnected[data.browserId]) {
+			alreadyConnected[data.browserId] = 0;
+			if (!streamViewers[data.streamKey]) {
+				streamViewers[data.streamKey] = 0;
+			}
+			streamViewers[data.streamKey]++;
+		}
+		alreadyConnected[data.browserId]++;
+		connectedSockets.push(socket);
+
+		socket.data = data;
+		io.emit(
+			`viewer_count_${data.streamKey}`,
+			streamViewers[data.streamKey],
+		);
+	});
+
+	socket.on("heartbeat", (data: { heartbeat: number }) => {
+		socket.data.heartbeat = data.heartbeat;
+	});
+
+	socket.on("disconnect", () => {
+		disconnectUser(io, socket);
+	});
+};
+
+export const checkHeartbeat = (io: Server) => {
+	setInterval(() => {
+		const now = Date.now();
+		connectedSockets.forEach((socket) => {
+			if (now - socket.data.heartbeat > 2000) {
+				logInfo(
+					"WS",
+					setInterval.name,
+					"No heartbeat received in the last 30 seconds. Assuming disconnected.",
+				);
+
+				disconnectUser(io, socket);
+			}
+		});
+	}, 1000);
+};
