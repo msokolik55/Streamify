d4e932d5329b3a554cb30c5ec2d1009a27e2caf5
Michal Sokolik <msokolik55@gmail.com>
2024-04-23 14:18:05 +0200

    feat: added Home to Drawer

--- frontend/src/components/drawer/MainDrawer.tsx
+++ frontend/src/components/drawer/MainDrawer.tsx
@@ -1,5 +1,7 @@
 import { Divider } from "primereact/divider";
+import { Menu } from "primereact/menu";
 import { Sidebar } from "primereact/sidebar";
+import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 
 import { isDrawerOpenedAtom } from "../../atom";
@@ -7,6 +9,7 @@ import { apiUserUrl, livePath, profilePath } from "../../urls";
 import SectionDrawer from "./SectionDrawer";
 
 const MainDrawer = () => {
+	const navigate = useNavigate();
 	const [isDrawerOpened, setIsDrawerOpened] =
 		useRecoilState(isDrawerOpenedAtom);
 
@@ -16,6 +19,21 @@ const MainDrawer = () => {
 
 	return (
 		<Sidebar visible={isDrawerOpened} onHide={toggleDrawer}>
+			<Menu
+				className="w-full"
+				model={[
+					{
+						label: "Home",
+						icon: "pi pi-home",
+						command: () => {
+							navigate("/");
+						},
+					},
+				]}
+			/>
+
+			<Divider />
+
 			<SectionDrawer
 				label="Live"
 				url={`${apiUserUrl}?live=true`}
