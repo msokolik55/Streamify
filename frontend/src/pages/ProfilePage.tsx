3e9e6bd0b0ca20ec4526e1718ac66578a3b83272
Michal Sokolik <msokolik55@gmail.com>
2024-04-23 15:45:06 +0200

    reafactor: using BrowseStreamsPanel in ProfilePage

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,9 +1,10 @@
 import { Helmet } from "react-helmet";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import useSWR from "swr";
 
+import BrowseStreamsPanel from "../components/BrowseStreamsPanel";
+import Heading1 from "../components/Heading1";
 import ProfilePicture from "../components/ProfilePicture";
-import StreamCard from "../components/StreamCard";
 import MainWindowError from "../components/errors/MainWindowError";
 import { IResponseData } from "../models/IResponseData";
 import { IUser } from "../models/IUser";
@@ -30,33 +31,21 @@ const ProfilePage = () => {
 	}
 
 	return (
-		<div className="px-8 pt-6 flex flex-col gap-8">
+		<div className="flex flex-col gap-4">
 			<Helmet>
 				<title>{user.username} - Streamify</title>
 			</Helmet>
 			<div className="flex flex-row gap-x-4 items-center">
 				<ProfilePicture src={user.picture} username={user.username} />
-				<h1 className="font-semibold text-2xl">{user.username}</h1>
+				<Heading1 title={user.username} />
 			</div>
 
-			<div className="flex flex-row gap-4 overflow-auto flex-wrap">
-				{user.streams.filter((stream) => stream.ended).length === 0 && (
-					<p>No videos to show.</p>
-				)}
-				{user.streams
-					.filter((stream) => stream.ended)
-					.map((stream, id) => (
-						<Link
-							key={`stream-${stream.name}-${id}`}
-							to={`${streamPath}/${stream.id}`}
-						>
-							<StreamCard
-								stream={stream}
-								username={user.username}
-							/>
-						</Link>
-					))}
-			</div>
+			<BrowseStreamsPanel
+				title="Videos"
+				streams={user.streams.filter((stream) => stream.ended)}
+				streamType="saved"
+				getUrl={(stream) => `${streamPath}/${stream.id}`}
+			/>
 		</div>
 	);
 };
