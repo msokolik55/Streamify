3f7003261efe157329dbc308e0aa3b6cc4fb426e
Michal Sokolik <msokolik55@gmail.com>
2024-04-25 22:13:57 +0200

    feat: added logging to auth.ts and login.ts

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,9 +1,12 @@
 import bcrypt from "bcrypt";
 import { Strategy as LocalStrategy } from "passport-local";
 
+import { logInfo } from "./logger";
 import { findByUsername, getPassword } from "./resources/user";
 
 export const authenticate = (passport: any) => {
+	logInfo("auth", authenticate.name, "Initializing passport.");
+
 	passport.use(
 		new LocalStrategy(async (username, password, done) => {
 			try {
@@ -30,12 +33,16 @@ export const authenticate = (passport: any) => {
 };
 
 export const serializeUser = (passport: any) => {
+	logInfo("auth", serializeUser.name, "Serializing user.");
+
 	passport.serializeUser((user: any, done: any) => {
 		done(null, user.username);
 	});
 };
 
 export const deserializeUser = (passport: any) => {
+	logInfo("auth", deserializeUser.name, "Deserializing user.");
+
 	passport.deserializeUser(async (username: string, done: any) => {
 		try {
 			const user = await findByUsername(username);
--- backend/src/resources/login.ts
+++ backend/src/resources/login.ts
@@ -1,18 +1,29 @@
 import { Request, Response } from "express";
 
+import { logInfo } from "../logger";
 import { Status, sendResponseError, sendResponseSuccess } from "./response";
 
 export const login = (_req: Request, res: Response) => {
+	logInfo("/login", login.name, "User logged in.");
+
 	return sendResponseSuccess(res, Status.OK, true);
 };
 
 export const logout = (req: Request, res: Response) => {
+	logInfo("/logout", logout.name, "User logged out.");
+
 	req.logout(() => {
 		res.redirect("/users/profile");
 	});
 };
 
 export const isAuthenticated = (req: Request, res: Response) => {
+	logInfo(
+		"/authenticated",
+		isAuthenticated.name,
+		"Checking if user is authenticated.",
+	);
+
 	if (req.isAuthenticated()) {
 		return sendResponseSuccess(res, Status.OK, req.session);
 	}
